6737662e0f02e0bab448f78a7eb027f1e3a39480
Mehul Arora <skrm@Mehuls-MacBook-Air.local>
2026-07-19 20:43:05 +0530

    Add React CI pipeline

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,4 @@
 module.exports = {
   presets: [['@babel/preset-env', {targets: {node: 'current'}}],
-             ['@babel/preset-reset', { runtime: "automic" }] ],
+             ['@babel/preset-react', { runtime: "automatic" }] ],
 };
